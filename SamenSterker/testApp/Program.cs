5751c4c31297d6914e6fb1ae6f3522c1712c5abc
pjfoi <pj_foi@hotmail.com>
2014-05-08 15:27:28 +0200

    db update

---
+++ SamenSterker/testApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SamenSterkerData.Company c1 = SamenSterkerData.CompanyDB.GetById(1);
+            Console.WriteLine(c1.Name);
+            Console.Read();
+        }
+    }
+}
