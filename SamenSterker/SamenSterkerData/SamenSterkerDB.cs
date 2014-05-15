806c30d8426fb1d28acc422c57f698b121eed8c1
peterneyens <peter.neyens@gmail.com>
2014-05-15 09:48:41 +0200

    start user interface with Modern UI

--- SamenSterker/SamenSterkerData/SamenSterkerDB.cs
+++ SamenSterker/SamenSterkerData/SamenSterkerDB.cs
@@ -12,7 +12,8 @@ namespace SamenSterkerData
         public static SqlConnection GetConnection()
         {
             string connectionString =
-            "Data Source=(localDB)\\v11.0;AttachDbFilename=D:\\software\\Dropbox\\Geïntegreerd project\\NET project\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
+            //"Data Source=(localDB)\\v11.0;AttachDbFilename=D:\\software\\Dropbox\\Geïntegreerd project\\NET project\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
+            "Data Source=(localDB)\\v11.0;AttachDbFilename=C:\\Users\\Peter\\Dropbox\\Geïntegreerd project\\NET project\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
             return connection;
         }
