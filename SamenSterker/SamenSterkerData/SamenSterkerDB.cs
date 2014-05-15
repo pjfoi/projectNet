ef1f0a917e0aedad0f915971c0f1ec9c6fd28f94
peterneyens <peter.neyens@gmail.com>
2014-05-15 16:20:54 +0200

    Further UI improvements

--- SamenSterker/SamenSterkerData/SamenSterkerDB.cs
+++ SamenSterker/SamenSterkerData/SamenSterkerDB.cs
@@ -12,8 +12,21 @@ namespace SamenSterkerData
         public static SqlConnection GetConnection()
         {
             string connectionString =
-            //"Data Source=(localDB)\\v11.0;AttachDbFilename=D:\\software\\Dropbox\\Geïntegreerd project\\NET project\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
             "Data Source=(localDB)\\v11.0;AttachDbFilename=C:\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
+
+            switch (System.Environment.MachineName)
+            {
+                case "PC_NEYENS":
+                    connectionString = @"Data Source=(localDB)\v11.0;AttachDbFilename=C:\Users\Peter\AB1_SQLserver_copy.mdf;Integrated Security=True";
+                    break;
+                case "PJ":
+                    connectionString = "Data Source=(localDB)\\v11.0;AttachDbFilename=D:\\software\\Dropbox\\Geïntegreerd project\\NET project\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
+                    break;
+                case "M-HP":
+                    connectionString = "Data Source=(localDB)\\v11.0;AttachDbFilename=C:\\DB\\AB1_SQLserver.mdf;Integrated Security=True";
+                    break;
+            }
+
             SqlConnection connection = new SqlConnection(connectionString);
             return connection;
         }
